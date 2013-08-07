a42b759bbee0a94513d899de32ee1001fd3d1d0e
Joshua Vaage <joshuavaage@gmail.com>
2013-08-07 14:09:49 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpenvintageCom::Application.config.secret_key_base = '07de41caf50363582a4a3c88b8990380a89598c17bfb2f7957dbc40abf2a09f764f277bc839951c490bbefe7a057465c2e663c1ba0ae9465aebb75cd3747eab6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpenvintageCom::Application.config.session_store :cookie_store, key: '_openvintage_com_session'
